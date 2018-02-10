cbb0a7623983175279dd19edda365d159b642412
landonrs <altruist8@yahoo.com>
2018-02-10 12:52:31 -0700

    added search by category

--- web/week06/object_library.php
+++ web/week06/object_library.php
@@ -4,8 +4,17 @@ session_start();
 $dbUrl = getenv('DATABASE_URL');
 $message = "";
 $db = Null;
+$search_category = $_REQUEST["category"];
+echo $search_category;
 if($_REQUEST["search"] !== null){
 $search_object = $_REQUEST['search'];
+// echo "<script> alert('search by name')</script>";
+$search_category = null;
+}
+else if($search_category !== null){
+	if($search_category == "all"){
+		$search_category = null;
+	}
 }
 
 if (empty($dbUrl)) {
@@ -58,26 +67,49 @@ include('navbar.php');
 ?>
 <div class="library-title"><h2 class="coming_soon">Current Object Library:</h2></div>
 <div class="search-bar"><form id="lookup" action="object_library.php" method="post">
-   <input type="text" name="search" form="lookup"><input type="submit" value="search"></form></div>
+   <input type="text" name="search" form="lookup"><input type="submit" value="search by name"></form></div>
+<div class='category-bar'><form id="c-search" action="object_library.php" method="get">
+   <select name="category" form="c-search">
+		<option value="all">all</option>
+				<?php
+				foreach ($db->query('SELECT CATEGORY_NAME FROM CATEGORIES') as $row)
+				{
+				  echo '<option value="' . $row['category_name'] . '"> ' . $row['category_name'] . ' </option>';
+				}
+				?>				
+		   </select>
+   <input type="submit" value="or search objects by category">
+</form></div>
 <div class="clear" style="overflow:auto;">
 <table class="table table-light">
     <tbody>
 		<?php 
-		if($search_object == Null){
-		try{
-			$statement = $db->query('SELECT object_name, object_image from objects');
-			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-			{
-			  echo '<tr><td>Object Name: ' . $row['object_name'] . '</td><td>Object Image:<img src="object_images\\' . $row['object_name'] . '.jpg"></td>';
-			  echo "<td><form action='object_details.php' method='get'><input type='hidden' name='name' value='".$row['object_name']."'><input type='submit' value='See object details'></td></form></tr>";
+		
+		if($search_category != Null){
+			try{
+			$statement = $db->prepare('SELECT OBJECT_NAME FROM OBJECTS O JOIN OBJECT_CATEGORIES OC ON O.OBJECT_ID = OC.OBJECT_ID 
+										JOIN CATEGORIES C ON C.CATEGORY_ID = OC.CATEGORY_ID
+										WHERE C.CATEGORY_NAME =:name');
+			$statement->bindValue(':name', $search_category, PDO::PARAM_STR);
+			$statement->execute();
+			if($statement->rowCount() > 0){
+				while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+				{
+				  echo '<tr><td>Object Name: ' . $row['object_name'] . '</td><td>Object Image:<img src="object_images\\' . $row['object_name'] . '.jpg"></td>';
+				  echo "<td><form action='object_details.php' method='get'><input type='hidden' name='name' value='".$row['object_name']."'><input type='submit' value='See object details'></td></form></tr>";
+				}
+			}
+			else{
+				echo "<h1 style='color:white;'>No object matched your category search.</h1>";
 			}
 		}catch (PDOException $ex)
 		{
 		  echo 'Error!: ' . $ex->getMessage();
 		  die();
 		}
+			
 		}
-		else{
+		else if($search_object != Null){
 			try{
 			$statement = $db->prepare('SELECT object_name, object_image from objects where object_name =:name');
 			$statement->bindValue(':name', $search_object, PDO::PARAM_STR);
@@ -97,9 +129,21 @@ include('navbar.php');
 		  echo 'Error!: ' . $ex->getMessage();
 		  die();
 		}
-			
 		}
-		
+		else{
+		try{
+			$statement = $db->query('SELECT object_name, object_image from objects');
+			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+			{
+			  echo '<tr><td>Object Name: ' . $row['object_name'] . '</td><td>Object Image:<img src="object_images\\' . $row['object_name'] . '.jpg"></td>';
+			  echo "<td><form action='object_details.php' method='get'><input type='hidden' name='name' value='".$row['object_name']."'><input type='submit' value='See object details'></td></form></tr>";
+			}
+		}catch (PDOException $ex)
+		{
+		  echo 'Error!: ' . $ex->getMessage();
+		  die();
+		}
+		}
 		?>
     </tbody>
   </table>
